5fded58a827807fa88c08cc8e43aaf0148202c10
Rohith Teja GMK <Rohith Teja GMK@GVRT95-GMK>
2016-07-06 14:10:02 +0530

    INIT

---
+++ YTS-Mark_1/YTS-Mark_1/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using YTS_Mark_1.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace YTS_Mark_1.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
